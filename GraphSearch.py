aefb578e621c7641de8eeecbf39453a6e8125b0a
Marco Di Pietro <dptr.marco@gmail.com>
2015-04-01 12:37:42 +0200

    Update GraphSearch.py

--- GraphSearch.py
+++ GraphSearch.py
@@ -2,7 +2,7 @@
 """
     Generalized Graph Search.
 
-    Deque OPEN e dizionario CLOSED.
+    Deque OPEN and dictionary CLOSED.
 """
 
 from collections import deque
@@ -17,31 +17,31 @@ def GraphSearch(problem):
     start = problem.start
     goal = problem.goal
 
-    #1 Crea una lista OPEN ed inserisci il nodo start.
+    #1 Create the list OPEN and push the start node.
     OPEN = deque()
     OPEN.append(start)
 
-    #2 Crea un dizionario CLOSED inizialmente vuoto.
+    #2 Create the empty dictionary CLOSED.
     CLOSED = {}
 
-    #3 Se OPEN e' vuota, esci con fallimento.
+    #3 If OPEN is empty, then exit.
     while OPEN:
-        #4 pop della coda OPEN
+        #4 pop from OPEN
         n = OPEN.popleft()
         c += 1
-        #5 inserisce n in CLOSED
+        #5 put n in CLOSED
         CLOSED[n.content] = n
 
-        #6 se il goal e' stato raggiunto, restituisci il percorso solutivo
+        #6 if the goal node has been reached, return the solution's path
         if n == goal:
             print 'GOAL!'
             return (problem.path(n, start), c)
 
-        #7 genera i successori del nodo n
+        #7 generate the node n successors'
         s = problem.successors(n)
 
-        #8 per ogni successore di n, se non e' un nodo goal
-        #oppure non e' in CLOSED, aggiungilo in OPEN
+        #8 for each successor of n, if it isn't a goal node
+        # or it is not in CLOSED, then push it into OPEN
         for n_first in s:
             if n_first == goal:
                 n_first.parent = n
